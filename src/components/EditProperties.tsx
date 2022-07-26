80846973ffe2732a1dd1410c3fb8c48ef3b81c12
Anastasia Brikun <brikun.a@aicrobotics.ru>
2022-07-26 11:03:08 +0300

    feat: selectors family

--- src/components/EditProperties.tsx
+++ src/components/EditProperties.tsx
@@ -1,46 +1,21 @@
 import {InputGroup, InputRightElement, NumberInput, NumberInputField, Text, VStack} from '@chakra-ui/react'
 import { useRecoilState, useRecoilValue } from 'recoil'
-import { selectedElementPropsSelector } from '../store/selectors/selectedElementPropsSelector'
+import { selectedElementAtom } from '../store/atoms/selectedElementAtom'
+import { editPropertyState } from '../store/selectors/editPropertyState'
 
 export const EditProperties = () => {
-    const [ element, setElement ] = useRecoilState(selectedElementPropsSelector)
-    
-    if (!element) return null
+    const selectedElement = useRecoilValue(selectedElementAtom)
 
-    const setPosition = (property: 'top' | 'left', value: number) => {
-        setElement({
-            ...element,
-            style: {
-                ...element.style,
-                position: {
-                    ...element.style.position,
-                    [property]: value
-                }
-            }
-        })
-    }
-
-    const setSize = (property: 'width' | 'height', value: number) => {
-        setElement({
-            ...element,
-            style: {
-                ...element.style,
-                size: {
-                    ...element.style.size,
-                    [property]: value
-                }
-            }
-        })
-    }
+    if (selectedElement === null) return null
     return (
         <Card>
             <Section heading="Position">
-                <Property label="Top" value={element.style.position.top} onChange={(top) => {setPosition('top', top) }} />
-                <Property label="Left" value={element.style.position.left} onChange={(left) => {setPosition('left', left)}} />
+                <Property label="Top" path='style.position.top' id={selectedElement} />
+                <Property label="Left" path='style.position.left' id={selectedElement}/>
             </Section>
             <Section heading="Size">
-                <Property label="Width" value={element.style.size.width} onChange={(width) => {setSize('width', width) }} />
-                <Property label="Height" value={element.style.size.height} onChange={(height) => {setSize('height', height)}} />
+                <Property label="Width" path='style.size.width' id={selectedElement}/>
+                <Property label="Height" path='style.size.height' id={selectedElement}/>
             </Section>
         </Card>
     )
@@ -55,14 +30,17 @@ const Section: React.FC<{heading: string}> = ({heading, children}) => {
     )
 }
 
-const Property = ({label, value, onChange}: {label: string; value: number; onChange: (value: number) => void}) => {
+const Property = ({label, path, id}: {label: string; path: string, id: number}) => {
+
+    const [value, setValue] = useRecoilState(editPropertyState({path, id}))
+
     return (
         <div>
             <Text fontSize="14px" fontWeight="500" mb="2px">
                 {label}
             </Text>
             <InputGroup size="sm" variant="filled">
-                <NumberInput value={value} onChange={(_, value) => onChange(value)}>
+                <NumberInput value={value} onChange={(_, value) => setValue(value)}>
                     <NumberInputField borderRadius="md" />
                     <InputRightElement pointerEvents="none" children="px" lineHeight="1" fontSize="12px" />
                 </NumberInput>
---
+++ src/store/selectors/editPropertyState.ts
@@ -0,0 +1,20 @@
+import { selectorFamily } from "recoil";
+import { elementAtom } from "../atoms/elementAtom";
+import { EDIT_PROPERTY } from "../keys";
+import { get as lodashGet, set as lodashSet } from "lodash";
+import produce from "immer";
+
+export const editPropertyState = selectorFamily<number, {path: string, id: number}> ({
+    key: EDIT_PROPERTY,
+    get: ({path, id}) => ({get}) => {
+        const element = get(elementAtom(id))
+        return lodashGet(element, path)
+    },
+    set: ({path, id}) => ({get, set}, newVal) => {
+        const element = get(elementAtom(id))
+        const newElement = produce(element, (draft) => {
+            lodashSet(draft, path, newVal)
+        })
+        set(elementAtom(id), newElement)
+    }
+})
